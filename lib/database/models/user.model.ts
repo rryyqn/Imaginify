d1a9e4d97a28654bd3665a40911806f7693547df
Ryan L <rliu2510@gmail.com>
2025-05-11 20:07:10 +1000

    initial

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,28 @@
+//clerkId, email, username, photo, firstName, lastName, planId, creditBalance
+
+import { model, models, Schema } from "mongoose";
+
+interface User {
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName: string;
+  lastName: string;
+  planId: string;
+  creditBalance: number;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  planId: { type: Number, default: 1 },
+  creditBalance: { type: Number, default: 10 },
+});
+
+const User = models?.User || model("User", UserSchema);
+export default User;
