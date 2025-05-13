0f9532cd517a2b606015a479ebfab6ae0c2270a4
Ryan L <rliu2510@gmail.com>
2025-05-13 22:25:07 +1000

    payment changes 5

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,22 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/clerk(.*)",
+  "/api/webhooks/stripe(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Match all API routes except webhooks
-    "/api/((?!webhooks/(clerk|stripe)).*)",
-    "/trpc/(.*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 };
